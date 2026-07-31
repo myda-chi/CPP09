9ab83163974345be03b9b23d6a89d491098c255d
Mepokam Okoye Yda Chioma <myda-chi@lab3r2s3.42abudhabi.ae>
2026-07-31 11:52:16 +0400

    d

---
+++ ex00/main.cpp
@@ -0,0 +1,16 @@
+#include "BitcoinExchange.hpp"
+
+int main(int argc, char* argv[])
+{
+    if (argc != 2)
+    {
+        std::cerr << "Usage: " << argv[0] << " <input_file>" << std::endl;
+        return 1;
+    }
+
+    BitcoinExchange bitcoinExchange;
+    bitcoinExchange.loadData("data.csv");
+    bitcoinExchange.processInput(argv[1]);
+
+    return 0;
+}
